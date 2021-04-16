ef78ec96b3475622e97b1b65adba6cee9c9f704a
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-16 17:03:22 -0300

    Objetos em JS
    O que são objetos em JS e como usá-los

---
+++ 3_objetos.js
@@ -0,0 +1,18 @@
+//OBJETOS
+//Objetos contém informações em pares de "chave: valor" e podem ser agrupados
+
+const pessoa = {
+    nome: "Glaucia",
+    idade: 37,
+    altura: 1.75,
+    peso: 80
+  };
+    console.log(pessoa); //acessa todas as informações do objeto pessoa
+  
+  //para acessar apenas 1 das informações do objeto usa-se:
+  //objeto.informação
+  
+  console.log(pessoa.idade); //acessa apenas a informação idade do objeto pessoa
+  console.log(pessoa.nome); //acessa apenas a informação nome do objeto pesso
+  console.log(pessoa.altura); //acessa apenas a informação altura do objeto pesso
+  console.log(pessoa.peso); //acessa apenas a informação peso do objeto pesso
