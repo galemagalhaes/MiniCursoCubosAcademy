38014e66cc384294ae78cea573039f757fcb723e
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-16 17:10:07 -0300

    Arrays
    O que é um array, para que usar um e como ele funciona.

---
+++ 7_arrays.js
@@ -0,0 +1,5 @@
+const listaDeCompras = ["arroz", "feijão", "batata", "maçã"];
+console.log(listaDeCompras[0]);
+console.log(listaDeCompras[1]);
+console.log(listaDeCompras[2]);
+console.log(listaDeCompras[3]);
