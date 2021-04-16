d34bd44e17f923bf75cf243427234d2ee6d8be90
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-16 17:01:13 -0300

    Introdução à lógica de programação
    "Olá Mundo!", declaração e atribuição de variáveis, booleano, operações matemáticas simples, incremento, atribuição + incremento, decremento, atribuição + decremento.

---
+++ 1_aulasMiniCurso.js
@@ -0,0 +1,61 @@
+console.log("Olá, mundo!");
+console.log("Olá, Fulano!!");
+
+let nome = "Glaucia";
+let idade = 37;
+
+console.log(nome);
+console.log(idade);
+
+nome = "Glaucia Alexandre";
+idade = null;
+
+console.log(nome);
+console.log(idade);
+
+//booleano
+const ehMaiorDeIdade = false;
+console.log(ehMaiorDeIdade);
+
+const resultadoSoma = 5 + 4;
+console.log(resultadoSoma);
+
+const resultadoMod = 5 % 4;
+console.log(resultadoMod);
+
+const resultadoSub = 5 - 4;
+console.log(resultadoSub);
+
+const resutadoDiv = 5 / 4;
+console.log(resutadoDiv);
+
+const resultadoMult = 5 * 4;
+console.log(resultadoMult);
+
+let resultado = 5;
+resultado = resultado + 5;
+console.log(resultado);
+
+//atribuir com soma
+resultado += 5;
+console.log(resultado);
+
+//atribuir com multiplicação
+resultado *= 5;
+console.log(resultado);
+
+//atribuir com divisão
+resultado /= 5;
+console.log(resultado);
+
+//atribuir com subtração
+resultado -= 5;
+console.log(resultado);
+
+//apenas para incrementar 1
+resultado ++;
+console.log(resultado);
+
+// apenas para decrementar 1
+resultado --;
+console.log(resultado);
