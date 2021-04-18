be1f4c63451aada72c44bee99e6fb74d96db6264
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-18 16:00:58 -0300

    While
    como usar o laço while

---
+++ 8_while.js
@@ -0,0 +1,7 @@
+//uso do laço de repetição while
+const listaDeCompras = ["arroz", "feijão", "batata", "macarrão"]
+let indice = 0
+while (indice < listaDeCompras.length){
+    console.log(listaDeCompras[indice]);
+        indice++;
+}
