0ac8c6de6195144c3948388c12305ab307851b73
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-26 09:55:53 -0300

    Acréscimo de comentários
    Comentários para identificar e explicar o funcionamento do código para minhas consultas futuras.

--- 14_funcaoComParametro.js
+++ 14_funcaoComParametro.js
@@ -1,7 +1,8 @@
 //Função com parâmetro
 
-function cumprimentarPessoa(nome) {
-  console.log("Olá, " + nome );
+function cumprimentarPessoa(nome) { //'nome' é passado como parâmetro, vai funcionar como uma pseudo variável
+  console.log("Olá, " + nome ); //nessa concatenação será impresso o que for passado como argumento do parâmetro nome
 }
 
-cumprimentarPessoa("Glaucia");
+cumprimentarPessoa("Glaucia"); //argumento "Glaucia"
+cumprimentarPessoa("Alexandre"); //argumento "Alexandre"
