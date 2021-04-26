c78192a9275ff055a2f0c965f5d750765670dac2
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-26 11:27:46 -0300

    Função para calcular desconto
    Mais um exercício resolvido, uma função para calcular desconto seguindo 3 regras, com variação de % de acordo com o valor da compra.

---
+++ 16_valorDescontoExercResolvido.js
@@ -0,0 +1,27 @@
+// A função deve imprimir um valor de desconto de acordo com algumas regras
+//o cupom deve ser RANGOBARATO;
+//deve ser usado entre 12h e 13:59h;
+//compras abaixo de 100 reais, desconto de 10%;
+//compras de 100 reais ou acima, desconto de 20%.
+
+function imprimirValorDesconto(valorDaCompra, hora, cupom){
+  let valorDesconto;
+  if(cupom === "RANGOBARATO") {
+    if(hora >= 12 && hora <= 14) {
+      if(valorDaCompra >= 100) {
+        valorDesconto = valorDaCompra * 20/100;
+      } else{
+        valorDesconto = valorDaCompra * 10/100
+      }
+      console.log(`O valor do desconto é ${valorDesconto}`);
+      //acrescentei a impressão do valor de pagamento da compra com desconto
+      console.log(`O valor a ser pago é ${valorDaCompra - valorDesconto}`)
+    } else {
+      console.log("Cupom não aceito neste horário")
+    }
+  } else {
+    console.log("Este cupom não é aplicável");
+  }
+}
+
+imprimirValorDesconto(150, 13, "RANGOBARATO");
