b9ca2a7d4e051407846058769ef394a87b522a9a
galemagalhaes <g.alemagalhaes@gmail.com>
2021-04-26 09:56:59 -0300

    correção

--- 14_funcaoComParametro.js
+++ 14_funcaoComParametro.js
@@ -1,6 +1,6 @@
 //Função com parâmetro
 
-function cumprimentarPessoa(nome) { //'nome' é passado como parâmetro, vai funcionar como uma pseudo variável
+function cumprimentarPessoa(nome) { //'nome' é passado como parâmetro, vai funcionar como uma pseudo variável que só existe dentro da função
   console.log("Olá, " + nome ); //nessa concatenação será impresso o que for passado como argumento do parâmetro nome
 }
 
